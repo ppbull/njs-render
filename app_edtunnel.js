e94bf14023fb4e6580459c86ee91574bd58c8d46
ppbull <35928760+ppbull@users.noreply.github.com>
2023-07-28 17:11:31 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,8 +4,8 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
-let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
+let userID = 'e6e5620a-5aff-4cb1-bc86-81006c743faa';
+let proxyIP = "64.68.192.119" + Math.floor(Math.random() * 255);
 
 let address = '';
 let portWithRandomLog = '';
